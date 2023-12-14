cac86bae77b4703dfb5d24fbdc1e1b6da0cfec65
Cuboost <121436798+cuboost@users.noreply.github.com>
2023-12-13 22:57:35 -0800

    Updated Dependencies

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,21 @@
-import Footer from './Footer';
-import Navbar from './Navbar';
-import './globals.css';
-import { Open_Sans } from 'next/font/google';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import "./globals.css";
+import { Open_Sans } from "next/font/google";
 
-const openSans = Open_Sans({ subsets: ['latin'] });
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Cuboost Movies',
-  description: 'Explore new movies!',
+  title: "Cuboost Movies",
+  description: "Explore new movies!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`flex min-h-screen flex-col items-center justify-between p-8 my-2 text-center ${openSans.className}`}>
+      <body
+        className={`flex min-h-screen flex-col items-center justify-between p-8 text-center ${openSans.className}`}
+      >
         <Navbar />
 
         {children}
